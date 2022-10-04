fcc64463dd0a8bcbe8dec05b4f6fc8aa3e01139c
aakash vishwakarma <ashv9730@gmail.com>
2022-10-04 18:33:38 +0530

    make home page

--- index.js
+++ index.js
@@ -1,43 +1,26 @@
+import express from "express";
+import expressejsLayouts from "express-ejs-layouts";
+import ejs from "ejs";
+import path from "path";
 
-import express from 'express'
-import expressejsLayouts from 'express-ejs-layouts'
-import ejs from 'ejs'
-import path from 'path'
+const app = express();
 
-const app = express()
+// static file
+app.use(express.static("public"));
 
-app.use('/static',express.static('public'))
-
-
-
-
-// ///set template
-// app.use(expressejsLayouts)
-// app.set('views', path.join(__dirname, "resources/views"))
-// app.set('view engine', 'ejs')
-
-
-// set Template engine
-// app.use(expressejsLayouts)
-app.set('views', path.join(__dirname, '/resources/views'))
-app.set('view engine', 'ejs')
+//set template
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
 
 app.use(expressejsLayouts);
 
-app.get("/", (req,res)=>{
-    
-    console.log('hii')
-    res.render('home.ejs')
-})
+app.get("/", (req, res) => {
+  
+  res.render("home");
+});
 
+const PORT = process.env.PORT || 3000;
 
-
-
-
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, ()=>{
-    console.log(`server listenning on port ${PORT}`)
-})
-
-
+app.listen(PORT, () => {
+  console.log(`server listenning on port ${PORT}`);
+});
